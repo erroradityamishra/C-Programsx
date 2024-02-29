747ca8b24529f596815eb7f22bd6b321d79208ca
Aditya Mishra <108148546+erroradityamishra@users.noreply.github.com>
2024-02-29 13:09:39 +0530

    Create Concatenate 2 strings.c

---
+++ Concatenate 2 strings.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+ char a[]= "Aditya ";
+ char b[] ="Mishra ";
+printf("Result: ");
+printf("\n");
+printf("%s",strcat(a,b));
+return 0;
+}
+
+
+// Result: 
+// Aditya Mishra 
